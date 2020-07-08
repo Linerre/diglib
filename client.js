254345d99001bb9fe4e850383769b7e5cccb7e96
Linerre <leon.errelin@outlook.com>
2020-07-08 10:32:52 +0800

    init done

---
+++ client.js
@@ -0,0 +1,8 @@
+// client js
+
+function checkOut()
+{
+	// var item = document.getElementById('123456789');
+	var status = document.getElementById('123456789-status').innerText;
+	status = 'Item checked out to you';
+}
