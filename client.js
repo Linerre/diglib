02c39269acc3fd1b1d7ea4991fb214ee7af9ae02
Linerre <leon.errelin@outlook.com>
2020-08-15 22:04:19 +0800

    update client js

--- client.js
+++ client.js
@@ -1,8 +1,57 @@
-// client js
+<script>
+    document.addEventListener('DOMContentLoaded', function(){
+      google.script.run.withSuccessHandler(statusUpdate).getStatus()
+    });
+     
+   
+    function statusUpdate(statusArray) {
+     var statusWeb = document.querySelectorAll('.status');
+     for (var i = 0; i < statusWeb.length; i++) {
+        if ((statusWeb[i].innerText = statusArray[i][1]) === 'Available') {
+           statusWeb[i].classList.add('avail');
+           document.querySelectorAll('.request')[i].innerText = 'Check Out';
+        } else {
+           statusWeb[i].classList.add('out');
+           document.querySelectorAll('.card-action')[i].classList.add('hide');
+        }
+      }
+    }
 
-function checkOut()
-{
-	// var item = document.getElementById('123456789');
-	document.getElementById('123456789-status').innerText = 'checkout to you';
-	document.getElementById('123456789-status').style.color = 'grey';
+
+//    $('.btn').click(function() {
+//       var barcode = this.id;
+//       alert(typeof barcode);
+//       
+//    });
+
+function openForm(clicked_id) {
+  document.getElementById("bg-m").style.display = 'flex';
+  document.getElementById("item-barcode").innerText = clicked_id;
 }
+
+function closeForm() {
+  document.getElementById("bg-m").style.display = 'none';
+}
+
+
+
+/*
+** submit request button actions
+** no php at the moment
+*/
+document.getElementById("sub").addEventListener("click", collectPatron);
+    
+function collectPatron(){
+    
+    var barcode = document.getElementById("item-barcode").innerText;
+    var netId = document.getElementById("netid").value;
+    var patron = document.getElementById("fname").value;
+    google.script.run.checkOut(barcode, netId);
+    closeForm();
+    
+    // reload the page with the annoying Apps Script
+    window.open("",'_top');
+}
+
+</script>
+
